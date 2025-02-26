0420d50535e4c9b37607b4ac6ef2d9e9271f0c9f
Simao <simaocampanico.dev@gmail.com>
2025-02-26 14:41:48 +0000

    Adding SmoothOperator project

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+
+            --n;
+            Console.WriteLine(n);
+            ++n;
+            Console.WriteLine(n);
+        }
+    }
+}
