4155256fbdbaa7e05132023ccc34e4166cbc530d
Simao <simaocampanico.dev@gmail.com>
2025-02-26 15:52:17 +0000

    Adding the volume and areaBase calculation

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -10,6 +10,10 @@ namespace Cilindro
             double a = double.Parse(Console.ReadLine());
             Console.Write("Raio do cilindro: ");
             double r = double.Parse(Console.ReadLine());
+            double volume = Math.PI * Math.Pow(r, 2) * a;
+            Console.WriteLine($"Volume do cilindro: {volume:F3} cm");
+            double area = 2 * Math.PI * r * (r + a);
+            Console.WriteLine($"Área da base do cilindro: {area:F3} cm²");
         }
     }
 }
