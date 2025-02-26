a83f86a6e2357c967ad5c7c10356fd246c828307
Simao <simaocampanico.dev@gmail.com>
2025-02-26 15:15:21 +0000

    Adding VariousOperators project

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro-não negativo: ");
+            byte n = byte.Parse(Console.ReadLine());
+
+            byte div = (byte)(n / 2);
+            Console.WriteLine(div);
+            byte shiftLeft = (byte)(n << 3);
+            Console.WriteLine(shiftLeft);
+            byte xor = (byte)(n ^ 6);
+            Console.WriteLine(xor);
+            bool isGreaterThanTen = n > 10;
+            Console.WriteLine(isGreaterThanTen);
+        }
+    }
+}
