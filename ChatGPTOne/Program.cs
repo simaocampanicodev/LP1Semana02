fd102a82954387320dd21a849e70242c2b82c5a3
Simao <simaocampanico.dev@gmail.com>
2025-02-27 16:24:39 +0000

    Adding ChatGPTOne project

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
