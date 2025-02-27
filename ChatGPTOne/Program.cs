c8291c5c20f1ad904af7bce3294f2064ffc81afa
Simao <simaocampanico.dev@gmail.com>
2025-02-27 22:54:12 +0000

    Adding 2 questions, not recognisable question and the exit

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace ChatGPTOne
+namespace ChatGPTZero
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            while (true)
+            {
+                Console.Write("Place your question (EXIT to leave): ");
+                string question = Console.ReadLine();
+                string response;
+
+                if (question.Equals("EXIT", StringComparison.OrdinalIgnoreCase))
+                {
+                    response = "Goodbye!";
+                    Console.WriteLine(response);
+                    break;
+                }
+                if (question == "What's your name?")
+                {
+                    response = "My name is ChatGPTOne.";
+                }
+                else if (question == "How are you?")
+                {
+                    response = "I'm good, thanks for asking!";
+                }
+                else
+                {
+                    response = "Sorry, I can't recognise your question, try again.";
+                }
+                Console.WriteLine(response);
+            }
         }
     }
 }
