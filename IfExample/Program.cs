b76016f5581bcf72844ba73acf7f81a91532417b
Simao <simaocampanico.dev@gmail.com>
2025-02-27 15:46:45 +0000

    Adding IfExample project

---
+++ IfExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace IfExample
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Caracteres a usar
+            char smile = '\u263A';
+            char frown = '\u2639';
+            char skull = '\u2620';
+
+            // Nave espacial
+            SpaceShip spaceShip = new SpaceShip();
+
+            // Estado da nave
+            bool shieldsUp = spaceShip.Shield > 0;
+            bool armourOk = spaceShip.Armour > 0;
+
+            // Linha de código necessária para mostrar caracteres Unicode
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Testar o if
+            if (shieldsUp && armourOk)
+            {
+                Console.WriteLine($"You're doing great {smile}");
+            }
+            else if (shieldsUp ^ armourOk)
+            {
+                Console.WriteLine($"You don't look so good {frown}");
+            }
+            else
+            {
+                Console.WriteLine($"You're dead {skull}");
+            }
+        }
+    }
+}
---
+++ IfExample/SpaceShip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IfExample
+{
+
+    public class SpaceShip
+    {
+
+        const int maxShield = 3;
+        const int maxArmour = 2;
+
+        private Random rng;
+
+        public SpaceShip()
+        {
+            rng = new Random();
+        }
+
+        public int Shield
+        {
+            get
+            {
+                return rng.Next(0, maxShield);
+            }
+        }
+
+        public int Armour
+        {
+            get
+            {
+                return rng.Next(0, maxArmour);
+            }
+        }
+    }
+}
